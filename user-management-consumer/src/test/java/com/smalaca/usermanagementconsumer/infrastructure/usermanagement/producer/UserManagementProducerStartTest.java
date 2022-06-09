e1033829f4fc7dd3bccea2bcf6be3590f5faba30
smalaca <ab13.krakow@gmail.com>
2022-06-09 20:46:33 +0200

    CDC - consumer for UM start functionality

--- user-management-consumer/src/test/java/com/smalaca/usermanagementconsumer/infrastructure/usermanagement/producer/UserManagementProducerStartTest.java
+++ user-management-consumer/src/test/java/com/smalaca/usermanagementconsumer/infrastructure/usermanagement/producer/UserManagementProducerStartTest.java
@@ -1,11 +1,20 @@
 package com.smalaca.usermanagementconsumer.infrastructure.usermanagement.producer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@AutoConfigureStubRunner(
+        ids = {"com.smalaca:user-management:+:stubs:8100"},
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class UserManagementProducerStartTest {
-    private final UserManagementProducer producer = new UserManagementProducerFactory().userManagementProducer();
+    @Autowired
+    private UserManagementProducer producer;
 
     @Test
     void shouldSayHelloToGuest() {
